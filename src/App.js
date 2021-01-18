17b5192111b9b07ae000c35c9e0432d5a5842330
Edyta2801 <edyta.szarowska@interia.pl>
2021-01-18 17:13:49 +0100

    npm install --save prop-types

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import React from 'react';
+
+class Button extends React.Component {
+  state = { color: 'blue' }
+
+  static efaultProps = {
+    label: 'OK'
+  }
+
+  render() {
+    return <button>{this.props.label}</button>
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <div>Hello tests !!!</div>
+      <Button label='Click me' />
     </div>
   );
 }
